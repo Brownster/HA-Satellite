6e5a4e299ff58802b0efbdb1930514e0e77ead1e
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-18 21:24:56 +0000

    Update border-overlay.py

--- border-overlay.py
+++ border-overlay.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import time
 
 # Function to overlay a colored border around the screen
 def overlay_border(color):
@@ -10,6 +11,11 @@ def overlay_border(color):
     command = f"DISPLAY=:0 xsetroot -solid {color}"
     subprocess.run(command, shell=True)
 
+# Function to remove the overlay after a specified delay
+def remove_overlay(timeout):
+    time.sleep(timeout)
+    subprocess.run("DISPLAY=:0 xsetroot -solid black", shell=True)
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python border_overlay.py <color>")
@@ -17,3 +23,7 @@ if __name__ == "__main__":
 
     color = sys.argv[1]
     overlay_border(color)
+
+    # Set the timeout (in seconds)
+    timeout = 3
+    remove_overlay(timeout)
