b1d439528d610eb178f0b3a194ac4e2143fc3742
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-19 21:57:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -229,6 +229,44 @@ for script in "${python_scripts[@]}"; do
     create_service "$script"
 done
 
+# Create a script to start a Python HTTP server and configure it as a systemd service
+
+# Step 1: Create the Python HTTP server script
+echo "Creating Python HTTP server script..."
+cat <<EOF > /usr/src/HA-Satellite/scripts/start_web_server.sh
+#!/bin/bash
+cd /usr/src/HA-Satellite/templates/main/
+python3 -m http.server 8000
+EOF
+
+# Make the script executable
+chmod +x /home/pi/start_web_server.sh
+
+# Step 2: Create a systemd service for the Python HTTP server
+echo "Creating systemd service for the Python HTTP server..."
+cat <<EOF > /etc/systemd/system/web-server.service
+[Unit]
+Description=Local Web Server
+After=network.target
+
+[Service]
+ExecStart=/usr/src/HA-Satellite/scripts/start_web_server.sh
+Restart=always
+User=pi
+Group=pi
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the systemd service to start at boot
+systemctl enable web-server.service
+
+# Start the service
+systemctl start web-server.service
+
+echo "Web server setup completed."
+
 # Reboot the satellite
 echo " rebooting the server"
 sudo reboot
