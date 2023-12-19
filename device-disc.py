8ac55fa172cba9f688b022d05e30faf3e7f7605a
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-19 22:56:36 +0000

    Update device-disc.py

--- device-disc.py
+++ device-disc.py
@@ -23,6 +23,23 @@ brightness_config = {
 
 client.publish(f"homeassistant/sensor/{device_name}/brightness/config", json.dumps(brightness_config), retain=True)
 
+
+# Define device information
+device_name = "your_alarm_clock"
+device_unique_id = "alarm_clock1"  # You can make this unique for each alarm clock
+device_state_topic = f"homeassistant/{device_name}/state"
+
+# Publish discovery message for alarm clock
+alarm_clock_config = {
+    "name": "Alarm Clock",
+    "unique_id": f"{device_unique_id}/alarm",
+    "state_topic": device_state_topic,
+    "icon": "mdi:clock",
+}
+
+client.publish(f"homeassistant/sensor/{device_name}/alarm/config", json.dumps(alarm_clock_config), retain=True)
+
+
 # Publish discovery message for screen border color
 border_color_config = {
     "name": "Screen Border Color",
