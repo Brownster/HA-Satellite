a3c146b46b95885a94752a9dc00606a33fe69c2d
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-19 21:45:46 +0000

    Update mqtt-listner.py

--- mqtt-listner.py
+++ mqtt-listner.py
@@ -4,10 +4,12 @@ import paho.mqtt.client as mqtt
 mqtt_host = "your_mqtt_broker_ip"
 mqtt_port = 1883
 topic = "homeassistant/your_screen/state"
+border_topic = "homeassistant/your_screen/border"  # Define a new topic for setting the border
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker with result code " + str(rc))
     client.subscribe(topic)
+    client.subscribe(border_topic)  # Subscribe to the new border topic
 
 def on_message(client, userdata, msg):
     message = msg.payload.decode("utf-8")
@@ -22,6 +24,22 @@ def on_message(client, userdata, msg):
             os.system(f"xbacklight -set {brightness}")  # This command works on Linux with xbacklight installed
         except ValueError:
             print("Received invalid brightness value")
+    elif msg.topic == border_topic:  # Check if the message is for setting the border
+        if message.lower() == "blue":
+            overlay_border("blue")
+        elif message.lower() == "red":
+            overlay_border("red")
+        else:
+            print("Received invalid border color")
+
+# Function to overlay a colored border around the screen
+def overlay_border(color):
+    if color not in ['blue', 'red', 'green', 'orange']:
+        print("Invalid color. Supported colors are 'blue', 'red', 'green', 'orange'.")
+        return
+
+    command = f"DISPLAY=:0 xsetroot -solid {color}"
+    os.system(command)
 
 client = mqtt.Client()
 client.on_connect = on_connect
