0830d6595624129cc66eb2701d6ec3e01603cc8c
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-20 20:18:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,8 +39,8 @@ sudo apt-get install --no-install-recommends git python3-venv florence pygame
 echo "Installing LXDE..."
 
 # Install Xorg and LXDE
-sudo apt-get install --no-install-recommends xserver-xorg
-sudo apt-get install lxde-core lxappearance
+sudo apt-get install --no-install-recommends xserver-xorg -y
+sudo apt-get install lxde-core lxappearance -y
 
 # Optional: Install LightDM (if you want a display manager / login screen)
 #sudo apt-get install lightdm
