128d2e6ac0016dec1762a3ce5c44f0bf6b1d1f03
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 20:44:00 +0000

    Update install_chromium_kiosk.sh

--- install/install_chromium_kiosk.sh
+++ install/install_chromium_kiosk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ################ INSTALL CHROMIUM IN KIOSK MODE #######################
+# Ask for the username
+read -p "Enter the username for auto-start setup: " username
+
 # Install Chromium
 echo "Installing Chromium..."
 sudo apt install chromium-browser -y
@@ -8,26 +11,30 @@ sudo apt install chromium-browser -y
 echo "Creating directory for Chromium start script..."
 sudo mkdir -p /usr/src/chromium
 
-# Create a script to launch Chromium in kiosk mode with mobile user agent
+# Create a script to launch Chromium in kiosk mode
 echo "Setting up script to start Chromium in kiosk mode"
 cat <<EOF > /usr/src/chromium/start-chromium.sh
 #!/bin/bash
 # Example user agent of a tablet device
 USER_AGENT="Mozilla/5.0 (Linux; Android 8.0; Pixel C Build/OPR1.170623.032) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
-chromium-browser --kiosk --user-agent="$USER_AGENT" --remote-debugging-port=9222 http://127.0.0.1:8000
+chromium-browser --kiosk --user-agent="\$USER_AGENT" --remote-debugging-port=9222 http://127.0.0.1:8000
 EOF
 
 # Make the Chromium start script executable
 sudo chmod +x /usr/src/chromium/start-chromium.sh
 
 # Auto Start Chromium in Kiosk Mode
-echo "Setting up Chromium to start in kiosk mode on boot..."
+echo "Setting up Chromium to start in kiosk mode on boot for user $username..."
 
-# Ensure the autostart directory exists
-mkdir -p ~/.config/lxsession/LXDE-pi/
+# Ensure the autostart directory exists for the specified user
+sudo mkdir -p /home/$username/.config/lxsession/LXDE-pi/
 
 # Write the commands to the autostart file
-echo "@lxpanel --profile LXDE-pi" > ~/.config/lxsession/LXDE-pi/autostart
-echo "@pcmanfm --desktop --profile LXDE-pi" >> ~/.config/lxsession/LXDE-pi/autostart
-echo "@/usr/src/chromium/start-chromium.sh" >> ~/.config/lxsession/LXDE-pi/autostart
-echo "@onboard -d" >> ~/.config/lxsession/LXDE-pi/autostart
+{
+    echo "@lxpanel --profile LXDE-pi"
+    echo "@pcmanfm --desktop --profile LXDE-pi"
+    echo "@/usr/src/chromium/start-chromium.sh"
+    echo "@onboard -d" # On-screen keyboard, remove if not needed
+} | sudo tee /home/$username/.config/lxsession/LXDE-pi/autostart
+
+echo "Chromium is set to start in kiosk mode on boot for user $username."
