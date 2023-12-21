de044c625b8a51e529ac728b5fc3128d5e16ac58
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 18:22:38 +0000

    Update and rename update.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# The URL of the GitHub repository
+REPO_URL="https://github.com/Brownster/HA-Satellite.git"
+# The directory name that GitHub will clone into
+REPO_DIR="HA-Satellite"
+
+# Check if Git is installed
+if ! command -v git &> /dev/null
+then
+    echo "Git is not installed. Installing Git..."
+    sudo apt-get update
+    sudo apt-get install git -y
+fi
+
+# Clone the repository
+git clone $REPO_URL
+
+# Change directory to the repository
+cd $REPO_DIR/install
+
+# Run the main.sh script
+./main.sh
