1e2962c7d0eb4a965b8b98f9a0f6527c4ed63016
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 15:12:15 +0000

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -19,8 +19,9 @@ echo "3. Install Wyoming Satellite"
 echo "4. Install Spotify Connect"
 echo "5. Setup MQTT Services"
 echo "6. Setup Python HTTP Server"
-echo "7. Install All Components"
-echo "Enter your choice (1-7): "
+echo "7. Clean up and reboot"
+echo "8. Install all components"
+echo "Enter your choice (1-8): "
 read choice
 
 case $choice in
@@ -30,12 +31,14 @@ case $choice in
   4) sh install_spotify_connect.sh;;
   5) sh setup_mqtt_services.sh;;
   6) sh setup_python_http_server.sh;;
-  7) sh install_lxde.sh
+  7) sh finish_and_reboot.sh;;
+  8) sh install_lxde.sh
      sh install_chromium_kiosk.sh
      sh install_wyoming_satellite.sh
      sh install_spotify_connect.sh
      sh setup_mqtt_services.sh
      sh setup_python_http_server.sh
+     sh finish_and_reboot.sh
      echo "All components have been installed.";;
   *) echo "Invalid option. Please enter a number between 1 and 7.";;
 esac
