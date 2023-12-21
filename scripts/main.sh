d9a5f480224c219d313b7821a2711cfa421d7035
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 14:57:22 +0000

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,6 +2,15 @@
 
 #!/bin/bash
 
+# Update and upgrade the system
+echo "Step 1: Updating and upgrading the system..."
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get clean
+sudo apt-get install --no-install-recommends git python3-venv florence pygame git apache2
+pip install paho-mqtt playsound
+
 # Main installation script
 echo "Installation script for multiple components. Please select an option:"
 echo "1. Install LXDE"
