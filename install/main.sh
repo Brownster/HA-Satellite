32b2a423c5eb87a115af5752b26ed5d70a4def40
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 22:10:58 +0000

    update

--- install/main.sh
+++ install/main.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#!/bin/bash
+echo "this script is no where near complete but none the less attempts to install chromium in kiosk mode HA satellite for voice,"
+echo "spotify connect, a alarm clock and simple gui. With mqtt as to have some level of control over the assistants hardware"
+#                                                                                                      
+#                  ___ ___                          _______             __       __               __   
+#                 |   Y   .-----.--------.-----.   |   _   .-----.-----|__.-----|  |_.---.-.-----|  |_ 
+#                 |.  1   |  _  |        |  -__|   |.  1   |__ --|__ --|  |__ --|   _|  _  |     |   _|
+#                 |.  _   |_____|__|__|__|_____|   |.  _   |_____|_____|__|_____|____|___._|__|__|____|
+#                 |:  |   |                        |:  |   |                                           
+#                 |::.|:. |                        |::.|:. |                                           
+#                 `--- ---'                        `--- ---'                                           
+#                        ___ ___       __                _______       __         __ __ __ __          
+#                       |   Y   .-----|__.----.-----.   |   _   .---.-|  |_.-----|  |  |__|  |_.-----. 
+#                       |.  |   |  _  |  |  __|  -__|   |   1___|  _  |   _|  -__|  |  |  |   _|  -__| 
+#                       |.  |   |_____|__|____|_____|   |____   |___._|____|_____|__|__|__|____|_____| 
+#                       |:  1   |                       |:  1   |                                      
+#                        \:.. ./                        |::.. . |                                      
+#                         `---'                         `-------'                                      
+#                                                                                                      
+
+
+
+
 
 # Function to display the menu
 show_menu() {
