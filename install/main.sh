061a679911f20d3219fd9e28a207ca15ed833d71
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 16:48:39 +0000

    Update main.sh

--- install/main.sh
+++ install/main.sh
@@ -15,22 +15,22 @@ echo "Enter your choice (0-8): "
 read choice
 
 case $choice in
-  0) sh update.sh;;
-  1) sh install_lxde.sh;;
-  2) sh install_chromium_kiosk.sh;;
-  3) sh install_wyoming_satellite.sh;;
-  4) sh install_spotify_connect.sh;;
-  5) sh setup_services.sh;;
-  6) sh setup_python_http_server.sh;;
-  7) sh finish_and_reboot.sh;;
-  8) sh update.sh
-     sh install_lxde.sh
-     sh install_chromium_kiosk.sh
-     sh install_wyoming_satellite.sh
-     sh install_spotify_connect.sh
-     sh setup_services.sh
-     sh setup_python_http_server.sh
-     sh finish_and_reboot.sh
+  0) bash update.sh;;
+  1) bash install_lxde.sh;;
+  2) bash install_chromium_kiosk.sh;;
+  3) bash install_wyoming_satellite.sh;;
+  4) bash install_spotify_connect.sh;;
+  5) bash setup_services.sh;;
+  6) bash setup_python_http_server.sh;;
+  7) bash finish_and_reboot.sh;;
+  8) bash update.sh
+     bash install_lxde.sh
+     bash install_chromium_kiosk.sh
+     bash install_wyoming_satellite.sh
+     bash install_spotify_connect.sh
+     bash setup_services.sh
+     bash setup_python_http_server.sh
+     bash finish_and_reboot.sh
      echo "All components have been installed.";;
   *) echo "Invalid option. Please enter a number between 0 and 8.";;
 esac
