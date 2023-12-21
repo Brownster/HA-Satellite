11236539abf9dde0afe47dfcc0c8caac5b20135f
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-21 12:58:59 +0000

    Update alarm-monitor.py

--- scripts/alarm-monitor.py
+++ scripts/alarm-monitor.py
@@ -10,15 +10,23 @@ def read_alarms_from_store(file_path):
 def play_alarm_sound(sound_file):
     playsound(sound_file)
 
+def read_alarms_from_file(file_path=ALARMS_FILE):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
 def alarm_monitor(file_path, sound_file):
     while True:
-        # Read the current set of alarms
-        alarms = read_alarms_from_store(file_path)
+        try:
+            alarms = read_alarms_from_file(file_path)
+        except FileNotFoundError:
+            alarms = []  # If the file does not exist, assume no alarms
         now = datetime.datetime.now().strftime("%H:%M")
         if now in alarms:
             play_alarm_sound(sound_file)
-            # Optionally, remove the alarm from the list if it should only play once
-        time.sleep(60)  # Check every minute
+            alarms.remove(now)  # Optionally, remove the alarm
+            save_alarms_to_file(alarms)  # Save the updated list of alarms back to file
+        time.sleep(60)  # Sleep for 60 seconds before checking again
+
 
 if __name__ == "__main__":
     alarm_file_path = 'alarms.json'  # Path to the JSON file where alarms are stored
