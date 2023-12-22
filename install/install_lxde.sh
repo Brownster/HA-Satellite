900dc6ccc4f133346c579d47bfc3ac1399aabdd5
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-22 00:14:46 +0000

    Update install_lxde.sh

--- install/install_lxde.sh
+++ install/install_lxde.sh
@@ -14,3 +14,4 @@ echo "Setting up auto-login to desktop as the 'pi' user..."
 sudo raspi-config nonint do_boot_behaviour B4
 
 echo "LXDE installation complete. The system will auto-login to desktop as the 'pi' user."
+echo "if you want to change the auto-login user, run 'sudo raspi-config' and select 'System Options' > 'Boot / Auto Login'"
