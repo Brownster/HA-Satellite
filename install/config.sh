9496a5bee6b6fd450be6db9d9713632309a47d0e
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-27 23:37:18 +0000

    Update config.sh

--- install/config.sh
+++ install/config.sh
@@ -1,4 +1,4 @@
 # config.sh
 INSTALL_PATH="/usr/src/HA-Satellite" # Path to the HA-Satellite repo directory
 DEFAULT_URL="http://127.0.0.1/index.html" # Default URL for the homepage
-CONFIG_FILE="$INSTALL_PATH/scripts/config.sh" # Path to the config.sh file
+CONFIG_FILE="$INSTALL_PATH/scripts/config.sh" # Path to the config.sh file that holds the variables for the installation scripts
