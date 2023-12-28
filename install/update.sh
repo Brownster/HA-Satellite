a8f75f6af2ed2b060fda35c84f02fc6469ec5fef
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-28 15:50:29 +0000

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -10,4 +10,6 @@ sudo apt clean
 
 echo "Installing necessary packages..."
 # Removed subprocess from the installation list
-sudo apt install --no-install-recommends git python3-venv python3 python3-pip python3-paho-mqtt python3-playsound apache2 onboard -y
+sudo apt install --no-install-recommends git python3-venv python3 python3-pip python3-paho-mqtt apache2 onboard -y
+
+#python3-playsound
