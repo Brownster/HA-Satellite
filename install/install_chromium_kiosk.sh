f496315c50efafac96bcbb4ae3cef0233e329d5b
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-28 16:01:51 +0000

    Update install_chromium_kiosk.sh

--- install/install_chromium_kiosk.sh
+++ install/install_chromium_kiosk.sh
@@ -10,8 +10,10 @@ sudo apt install chromium-browser -y
 # Create the directory for the Chromium start script
 sudo mkdir -p /usr/src/chromium
 
+sudo touch /usr/src/chromium/start-chromium.sh
+
 # Create a script to launch Chromium in kiosk mode
-cat <<EOF > /usr/src/chromium/start-chromium.sh
+sudo cat <<EOF > /usr/src/chromium/start-chromium.sh
 #!/bin/bash
 USER_AGENT="Mozilla/5.0 (Linux; Android 8.0; Pixel C Build/OPR1.170623.032) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
 chromium-browser --kiosk --user-agent="\$USER_AGENT" --remote-debugging-port=9222 http://127.0.0.1:8000
