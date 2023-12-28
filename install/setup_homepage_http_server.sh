0e303d534e24a4f20ea126a68d33a0d111c2d083
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-28 16:58:55 +0000

    update

--- install/setup_homepage_http_server.sh
+++ install/setup_homepage_http_server.sh
@@ -2,11 +2,11 @@
 #The Home page is available at http://localhost/index.html
 source "$(dirname "$0")/config.sh"
 
-sudo mkdir -p /var/www/html/static/
-sudo rm -rf /var/www/html/index.html
-sudo cp $INSTALL_PATH/scripts/static/main-static/* /var/www/html/static
-sudo cp $INSTALL_PATH/scripts/templates/home-index.html /var/www/html/index.html
-sudo cp $INSTALL_PATH/scripts/templates/settings.html /var/www/html/settings.html
+sudo mkdir -p $WEBHOST/static/
+sudo rm -rf $WEBHOST/index.html
+sudo cp $INSTALL_PATH/scripts/static/main-static/* $WEBHOST/static
+sudo cp $INSTALL_PATH/scripts/templates/home-index.html $WEBHOST/index.html
+sudo cp $INSTALL_PATH/scripts/templates/settings.html $WEBHOST/settings.html
 
 echo "Home Page setup completed."
 echo "The Home page is available at http://localhost/index.html"
--- install/setup_services.sh
+++ install/setup_services.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 ################## Install scripts for MQTT etc and make them services.#######################
 source "$(dirname "$0")/config.sh"
-cd /usr/src
+cd $INSTALL_DIR
 if [ ! -d "HA-Satellite" ]; then
     sudo git clone https://github.com/Brownster/HA-Satellite
 fi
-cd /usr/src/HA-Satellite/scripts
+cd $INSTALL_PATH/scripts
 
-# Create the 'hasatellite' group and user for running the scripts
-echo "Creating group and user 'hasatellite' for running the scripts..."
-if ! id "hasatellite" &>/dev/null; then
-    echo "Creating group and user 'hasatellite' for running the scripts..."
-    sudo groupadd -f hasatellite
-    sudo useradd -r -M -g hasatellite -s /bin/false hasatellite
+# Create the '$USER_GROUP' group and user for running the scripts
+echo "Creating group and user '$USER_GROUP' for running the scripts..."
+if ! id "$USER_GROUP" &>/dev/null; then
+    echo "Creating group and user '$USER_GROUP' for running the scripts..."
+    sudo groupadd -f $USER_GROUP
+    sudo useradd -r -M -g $USER_GROUP -s /bin/false $USER
 else
-    echo "User 'hasatellite' already exists. Skipping user creation."
+    echo "User '$USER' already exists. Skipping user creation."
 fi
 
 
@@ -34,8 +34,8 @@ Wants=network.target
 [Service]
 ExecStart=/usr/bin/python3 "$PWD/$script_name"
 Restart=always
-User=hasatellite
-Group=hasatellite
+User=$USER
+Group=$USER_GROUP
 WorkingDirectory=$PWD
 StandardOutput=journal
 
