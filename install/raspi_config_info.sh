bb9780e7d3b83774d6e7694456b333d974168306
Marc Brown <marcsmobile.brown@googlemail.com>
2023-12-30 01:22:06 +0000

    Update raspi_config_info.sh

---
+++ install/raspi_config_info.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "----------------------------------------------------------------"
+echo "Raspberry Pi Configuration Instructions"
+echo "----------------------------------------------------------------"
+echo "You will next be presented with the 'raspi-config' tool."
+echo "To set your user to auto-login to the GUI, follow these steps:"
+echo "1. Run the 'raspi-config' tool:"
+echo "   Type 'sudo raspi-config' in the terminal and press Enter."
+echo ""
+echo "2. Navigate to the 'System Options' menu."
+echo ""
+echo "3. Select the 'Boot / Auto Login' option."
+echo ""
+echo "4. Choose 'Desktop Autologin' to enable automatic login to the GUI."
+echo ""
+echo "5. Finish up and exit 'raspi-config'. The changes will take effect after a reboot."
+echo "   Please DO NOT choose reboot now in the 'raspi-config' tool when exiting."
+echo ""
+echo "Loading raspi-config in 15 seconds..."
+echo "----------------------------------------------------------------"
+pause 15 seconds
+
+
